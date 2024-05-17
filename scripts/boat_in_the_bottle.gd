d9e99e7ea62ca8449aef9128b80eb2fe74a933e3
KiraYam <loicding93500@hotmail.com>
2024-05-17 15:13:57 +0200

    [fix] bug + [add] build

--- scripts/boat_in_the_bottle.gd
+++ scripts/boat_in_the_bottle.gd
@@ -23,6 +23,16 @@ func _on_upndown_area_entered(area):
 		$Lose.play()
 		await $Timer.timeout
 
+func _on_upndown_2_area_entered(area):
+	print("collision")
+	if (area == $Boat):
+		GameController.life -= 1
+		GameController.winning = false
+		$Boat.can_grab = false
+		$Boat.one_time = false
+		$Fail.visible = true
+		$Lose.play()
+		await $Timer.timeout
 
 func _on_timer_timeout():
 	if ($Boat.one_time):
